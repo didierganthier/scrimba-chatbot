3af48e2c8d19d5fb3324d1607a8d087911f00153
Didier Peran Ganthier <didierganthierperan@gmail.com>
2023-12-02 08:15:23 -0500

    Edit chunkSize

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
+import { RecursiveCharacterTextSplitter } from 'langchain/text_splitter'
+
 try {
-    const result = await fetch('scrimba-info.txt')
-    const text = await result.text()
-  } catch (err) {
-    console.log(err)
-  }
+  const result = await fetch('scrimba-info.txt')
+  const text = await result.text()
+  
+  const splitter = new RecursiveCharacterTextSplitter({
+    chunkSize: 500,
+  })
+  
+  const output = await splitter.createDocuments([text])
+  console.log(output)
+} catch (err) {
+  console.log(err)
+}
